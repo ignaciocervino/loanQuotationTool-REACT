bb2c81fddac96e6015341754d446d4671d2264e7
ignaciocervino <ignaciocervino@gmail.com>
2021-07-02 01:49:18 -0300

    Creating project

---
+++ src/helpers.js
@@ -0,0 +1,16 @@
+
+export function calcularTotal(cantidad,plazo){
+    let totalCantidad;
+    if (cantidad <= 1000) {
+        totalCantidad = cantidad*.25;
+    }else if (cantidad>1000 && cantidad<=5000) {
+        totalCantidad = cantidad * .20;
+    }else if (cantidad>5000 && cantidad<=10000) {
+        totalCantidad = cantidad * .15;
+    }
+    else{
+        totalCantidad = cantidad * .10;
+    }
+
+    //Calcular el plazo
+}
